7e11afe84e86df9462783ac1eaef20b43bd2f621
Alex <alexchbeir@hotmail.com>
2022-07-09 17:32:40 +0300

    Initial Commit

---
+++ Enemy Loses/Enemy_LosesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enemy_LosesApp.swift
+//  Enemy Loses
+//
+//  Created by Alex Shbeir on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Enemy_LosesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
